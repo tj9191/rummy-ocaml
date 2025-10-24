be8ee9014572dc79e5b2f02b3adc51911ca768f4
Tylan Joshi <tylanmoran@Tylans-MacBook-Pro.local>
2025-10-24 13:23:22 -0400

    Use phone-build as docs for GitHub Pages

--- _build/default/src/.rummy_engine.objs/rummy_engine__Ai.impl.d
+++
@@ -1,1 +0,0 @@
-src/ai.pp.ml: Array Core Engine Float List Option Ppx_bench_lib Ppx_expect_runtime Ppx_inline_test_lib Ppx_module_timer_runtime Random Rules Time_ns Types
--- _build/default/src/.rummy_engine.objs/rummy_engine__Engine.impl.d
+++
@@ -1,1 +0,0 @@
-src/engine.pp.ml: Array List Ppx_bench_lib Ppx_expect_runtime Ppx_inline_test_lib Ppx_module_timer_runtime Result Rules Types
